8ed831a45776a8e4e40b38e4169c0fcea0980160
zeeracha <zeeraratcha@gmail.com>
2022-12-25 20:29:30 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import './App.css'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  const fetchData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const convertData = await res.json(); //แปลงจาก json เป็น object{}
+    setData(convertData);
+
+    console.log("res",res);
+    console.log("convertData", convertData)
+  }
+
+  useEffect (() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+    {data.map(value => (
+      <div className='info' key={value.id}>
+        <h3>{value.id}.{value.title}</h3>
+        <img src={value.thumbnailUrl} alt={value.title} />
+      </div>
+    ))}
+   </div>
   );
 }
 
